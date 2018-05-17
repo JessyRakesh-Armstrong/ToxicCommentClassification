cf399d1c09771a90a26f6eea27137719cc282c9a
Jonome <jlam3214@gmail.com>
2018-05-17 03:59:19 -0700

    Added mean squared error

--- nb_model.py
+++ nb_model.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 train = pd.read_csv('train.csv')
-train['none'] = 1-train[labels].max(axis=1)
 
+x_train, x_valid = train_test_split(train, test_size=.3)
 vec=TfidfVectorizer()
-trn_term_doc = vec.fit_transform(train['comment_text'])
-x = trn_term_doc
+tfidf_vectors = vec.fit_transform(x_train['comment_text'])
+validation_vectors = vec.transform(x_valid['comment_text'])
+x = tfidf_vectors
+y = validation_vectors
 
 def pr(y_i, y):
     p = x[y==y_i].sum(0)
@@ -24,18 +27,26 @@ def get_mdl(y):
     x_nb = x.multiply(r)
     return m.fit(x_nb, y), r
 
-preds = np.zeros((len(train), len(labels)))
-for i, j in enumerate(labels):
-    print('fit', j)
-    m,r = get_mdl(train[j])
-    preds[:,i] = m.predict_proba(x.multiply(r))[:,1]
+preds = np.zeros((len(x_valid), len(labels)))
+for i,j in enumerate(labels):
+    print('fit',j)
+    m,r = get_mdl(x_train[j])
+    preds[:,i] = m.predict_proba(validation_vectors.multiply(r))[:,1]
 
-#x = data['comment_text']
-#CV = CountVectorizer()
-#count = CV.fit_transform(x)
-#tf_transformer = TfidfTransformer(use_idf=False).fit(count)
-#tf = tf_transformer.transform(count)
+train_data = x_valid
+train_data = train_data.drop(['id','comment_text'],axis=1)
+train_data = train_data.values
 
-#clf = MultinomialNB()
-#category = ['toxic', 'severe_toxic', ]
-#clf.fit(count,)
+print mean_square_error(train_data, preds)
+
+#preds = np.zeros((len(train), len(labels)))
+#for i, j in enumerate(labels):
+#    print('fit', j)
+#    m,r = get_mdl(train[j])
+#    preds[:,i] = m.predict_proba(x.multiply(r))[:,1]
+
+#train_data = train
+#train_data.drop(['id','comment_text'], axis=1)
+#train_data = train_data.values
+
+#print explained_variance_score(train_data, preds)
