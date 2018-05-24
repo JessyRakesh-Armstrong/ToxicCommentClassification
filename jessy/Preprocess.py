bd3163bbb4de5b91d315d30e395a24a8019c960e
JessyRakesh-Armstrong <jessyarmstrong761@gmail.com>
2018-05-24 15:52:06 -0700

    Jessy's preprocessing files

---
+++ jessy/Preprocess.py
@@ -0,0 +1,242 @@
+import pandas as pd
+import numpy as np
+import csv
+import copy
+import re
+from nltk import WordNetLemmatizer
+
+
+RE_PATTERNS = {
+    ' american ':
+        [
+            'amerikan'
+        ],
+
+    ' adolf ':
+        [
+            'adolf'
+        ],
+
+
+    ' hitler ':
+        [
+            'hitler'
+        ],
+
+    ' fuck':
+        [
+            '(f)(u|[^a-z0-9 ])(c|[^a-z0-9 ])(k|[^a-z0-9 ])([^ ])*',
+            '(f)([^a-z]*)(u)([^a-z]*)(c)([^a-z]*)(k)',
+            ' f[!@#\$%\^\&\*]*u[!@#\$%\^&\*]*k', 'f u u c',
+            '(f)(c|[^a-z ])(u|[^a-z ])(k)', r'f\*',
+            'feck ', 'fook', ' fux ', 'f\*\*',
+            'f\-ing', 'f\.u\.', 'f###', ' fu ', 'f@ck', 'f u c k', 'f uck', 'f ck'
+        ],
+
+    ' ass ':
+        [
+            '[^a-z]ass ', '[^a-z]azz ', 'arrse', ' arse ', '@\$\$'
+              '[^a-z]anus', ' a\*s\*s', '[^a-z]ass[^a-z ]',
+            'a[@#\$%\^&\*][@#\$%\^&\*]', '[^a-z]anal ', 'a s s'
+        ],
+
+    ' ass hole ':
+        [
+            ' a[s|z]*wipe', 'a[s|z]*[w]*h[o|0]+[l]*e', '@\$\$hole'
+        ],
+
+    ' bitch ':
+        [
+            'b[w]*i[t]*ch', 'b!tch',
+            'bi\+ch', 'b!\+ch', '(b)([^a-z]*)(i)([^a-z]*)(t)([^a-z]*)(c)([^a-z]*)(h)',
+            'biatch', 'bi\*\*h', 'bytch', 'b i t c h'
+        ],
+
+    ' bastard ':
+        [
+            'ba[s|z]+t[e|a]+rd'
+        ],
+
+    ' trans gender':
+        [
+            'transgender'
+        ],
+
+    ' gay ':
+        [
+            'gay'
+        ],
+
+    ' gay boy':
+        [
+            'gay boi'
+        ],
+
+    ' cock ':
+        [
+            '[^a-z]cock', 'c0ck', '[^a-z]cok ', 'c0k', '[^a-z]cok[^aeiou]', ' cawk',
+            '(c)([^a-z ])(o)([^a-z ]*)(c)([^a-z ]*)(k)', 'c o c k'
+        ],
+
+    ' dick ':
+        [
+            ' dick[^aeiou]', 'deek', 'd i c k'
+        ],
+
+    ' suck ':
+        [
+            'sucker', '(s)([^a-z ]*)(u)([^a-z ]*)(c)([^a-z ]*)(k)', 'sucks', '5uck', 's u c k'
+        ],
+
+    ' cunt ':
+        [
+            'cunt', 'c u n t'
+        ],
+
+    ' bull shit ':
+        [
+            'bullsh\*t', 'bull\$hit','bullshit'
+        ],
+
+    ' homo sex ual':
+        [
+            'homosexual', 'homo',
+        ],
+
+    ' jerk ':
+        [
+            'jerk'
+        ],
+
+    ' idiot ':
+        [
+            'i[d]+io[t]+', '(i)([^a-z ]*)(d)([^a-z ]*)(i)([^a-z ]*)(o)([^a-z ]*)(t)', 'idiots'
+                                                                                      'i d i o t'
+        ],
+
+    ' dumb ':
+        [
+            '(d)([^a-z ]*)(u)([^a-z ]*)(m)([^a-z ]*)(b)'
+        ],
+
+    ' shit ':
+        [
+            'shitty', '(s)([^a-z ]*)(h)([^a-z ]*)(i)([^a-z ]*)(t)', 'shite', '\$hit', 's h i t'
+        ],
+
+    ' shit hole ':
+        [
+            'shythole'
+        ],
+
+    ' retard ':
+        [
+            'returd', 'retad', 'retard', 'wiktard', 'wikitud'
+        ],
+
+    ' rape ':
+        [
+            ' raped'
+        ],
+
+    ' dumb ass':
+        [
+            'dumbass', 'dubass'
+        ],
+
+    ' ass head':
+        [
+            'butthead'
+        ],
+
+    ' sex ':
+        [
+            'sexy', 's3x', 'sexuality'
+        ],
+
+
+    ' nigger ':
+        [
+            'nigger', 'ni[g]+a', ' nigr ', 'negrito', 'niguh', 'n3gr', 'n i g g e r'
+        ],
+
+    ' shut the fuck up':
+        [
+            'stfu'
+        ],
+
+    ' pussy ':
+        [
+            'pussy[^c]', 'pusy', 'pussi[^l]', 'pusses'
+        ],
+
+    ' faggot ':
+        [
+            'faggot', ' fa[g]+[s]*[^a-z ]', 'fagot', 'f a g g o t', 'faggit',
+            '(f)([^a-z ]*)(a)([^a-z ]*)([g]+)([^a-z ]*)(o)([^a-z ]*)(t)', 'fau[g]+ot', 'fae[g]+ot',
+        ],
+
+    ' mother fucker':
+        [
+            ' motha ', ' motha f', ' mother f', 'motherucker',
+        ],
+
+    ' whore ':
+        [
+            'wh\*\*\*', 'w h o r e', 'whor', 'hore'
+        ],
+}
+
+"""
+This object will normalize a stringself.
+In this case of normalize we will get rid
+of stop words and use a patter we found online
+to reduce repititions and match common misspellings
+to easier to get words associated with their
+respcitve labels
+"""
+
+class Processor:
+
+    def __init__(self,initial_filters=r"\W",
+                patterns=RE_PATTERNS):
+        self.initial_filters = initial_filters
+        self.patterns = patterns
+
+    def normalize(self,text):
+        datatext = copy.deepcopy(text)
+
+        datatext = datatext.lower() #lowercase
+        #datatext = datatext.split(#rid of special characters & repititions
+        datatext = nltk.tokenize(datatext)
+        for n,word in enumerate(datatext):
+            cleaned_word = re.sub(self.initial_filters,'',word)
+            datatext[n] = cleaned_word
+        datatext = " ".join(datatext)
+
+
+
+
+
+        return datatext
+
+
+########################################################
+####### Processes begin#################################
+########################################################
+
+train = pd.read_csv("../data/train.csv")
+test = pd.read_csv("../data/test.csv")
+
+labels = ['toxic', 'severe_toxic', 'obscene',
+         'threat', 'insult', 'identity_hate']
+
+#New file to write to
+with open('fixed_Train.csv','w',newline='') as csvfile:
+    trainwriter = csv.DictWriter(csvfile,fieldnames=labels)
+    trainwriter.writeheader()
+
+fixer = Processor()
+a = "You are a FUCK!NG bastard!!!! "
+b = fixer.normalize(a)
+print(b)
